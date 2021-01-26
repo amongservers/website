41b793c9debea7cecf64af2dd357c13ab8a3fa45
Jonathan Madeley <jm@jonathanmadeley.com>
2021-01-26 22:20:39 +0000

    Initial commit
    - Designed site
    - Added generic content
    - Added logo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # website
 The launcher website
+
+
+## Style
+This project uses SCSS, to update the style run this command in the project directory.
+```
+sass Assets/SCSS/site.scss:wwwroot/css/site.min.css --watch --style=compressed --no-source-map
+```
---
+++ src/AmongServers.Website/Controllers/MainController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmongServers.Website.Controllers
+{
+    /// <summary>
+    /// Main controller.
+    /// </summary>
+    public class MainController : Controller
+    {
+        /// <summary>
+        /// Index page
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("/")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
